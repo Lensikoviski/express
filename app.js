e729fd2928717fd1f4bc58085ebda3f23d095acc
Lensikoviski <lensikoviski@gmail.com>
2022-12-10 08:39:10 +0530

    fifth commit

--- app.js
+++ app.js
@@ -2,17 +2,87 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 
 var {engine} = require('express-handlebars')
-app.engine('hbs',engine({extname:'hbs',defaultLayout:'default', layoutsDir: path.join(__dirname, '/views/layouts'),partialsDir:path.join(__dirname, 'views/partials')}))
+app.engine('hbs',engine({extname:'hbs',defaultLayout:'default', layoutsDir: path.join(__dirname, '/views/layouts'),partialsDir:path.join(__dirname, 'views/partials'),helpers:{
+  check:function(array,id,options){
+    //  console.log("array is",array)
+    //  console.log("id is ",id)
+      let found = false
+      for(i=0;i<array.length;i++){
+          
+          if(id.toString()==array[i].userid.toString()){
+              if(array[i].accepted==true){
+                  
+                  found = true
+                  console.log("view profile")
+                  return options.fn({viewprofile:true})
+                  break
+              }else{
+                 
+                  found = true
+                  console.log("intrest send")
+                  return options.fn({intrestsend:true})
+                  break
+              }
+          }
+
+      }  
+      if(!found){ 
+          console.log("i am intrested")
+          return options.fn({iamintrested:true})
+      }
+   
+  },
+  ifiacceptedornot:function(array,requested_user_id,options){
+      for(i=0;i<array.length;i++){
+          if(array[i].userid.toString()==requested_user_id.toString()){
+              if(array[i].accepted==true){ 
+                  return options.fn({
+                      accepted:true
+                  })
+              }else{
+                  return options.fn({
+                      accept:true
+                  })
+              }
+          }
+      }
+  },
+  ifheacceptedornot:function(intersted_array,curr_user_id,options){
+    console.log(intersted_array)
+    console.log(curr_user_id)
+      for(i=0;i<intersted_array.length;i++){
+          if(intersted_array[i].userid.toString()==curr_user_id.toString()){
+              if(intersted_array[i].accepted==true){
+                  return options.fn({
+                      accepted:true
+                  })
+              }
+              else if(intersted_array[i].declined==true){
+                  console.log("entered")
+                  return options.fn({
+                      declined:true  
+                  })
+              }
+              else{
+                  return options.fn({
+                      accept:true 
+                  })
+              }
+          }
+      }
+  }
+}}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -20,25 +90,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const oneday=24*60*60*1000
+app.use(session({secret:'key',cookie:{maxAge:oneday},resave:false,saveUninitialized:true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter); 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,99 @@ const { MongoClient } = require('mongodb');
 const uri="mongodb+srv://jayaramskumar:just4marry@cluster0.gguofay.mongodb.net/matrimony?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
 
+const verifylogin = (req,res,next)=>{
+  if(req.session.loggedin){
+    next()
+  }else{
+    res.render('layouts/signin')
+  }
+}
+
+/* GET home page. */
+router.get('/', function(req,res){
+  res.render('layouts/signin')
+});          
+
+router.get('/signin',verifylogin,(req,res)=>{
+  res.render('layouts/signin')
+})
+
+router.get('/signup',(req,res)=>{
+  //Dont put /layouts here
+  if(req.session.loggedin){
+    res.redirect('/')
+  }else{
+    res.render("layouts/signup")
+  }
+})
+
+  
+router.post('/signup',(req,res)=>{
+  client.db().collection('logininfo').insertOne(req.body)
+  res.redirect('/signin')
+  
+})   
+
+router.post('/signin',async(req,res)=>{
+  var pass = req.body.password
+  let user = await client.db().collection('logininfo').findOne({name:req.body.name})  
+  if(user){
+    if(user.password==pass){
+      console.log("user loged in")
+      req.session.username=user.name
+      req.session.loggedin=true
+      req.session.loginid=user._id
+     
+      res.redirect('/home')
+
+    }else{
+      console.log("incorrect password")
+      res.render('layouts/signin',{passErr:"incorrect password"})
+    }
+  }else{
+    console.log("incorrect username")
+    res.render('layouts/signin',{nameErr:"incorrect username"})
+  }
+
+})
 
 
-router.get('/',async function(req, res, next) {
-  let data =  await client.db().collection('userinfo').findOne({name:'akhil U Nair'})
-  res.render('layouts/home', { title: 'Express' ,data:data});
-});
+router.get('/home',verifylogin,async(req,res)=>{
+  let ifprofile = await client.db().collection('userinfo').findOne({
+    loginid:req.session.loginid.toString()
+  })
+  if(ifprofile){
+       let allusers =await client.db().collection("userinfo").find({
+        loginid:{
+           $nin:[req.session.loginid]
+        }
+        }).toArray()
+
+        let currdata = await client.db().collection('userinfo').findOne({
+            loginid:req.session.loginid
+        })
+        
+      
+ 
+        let curr_user_id=''
+        if(currdata){
+          curr_user_id = currdata._id 
+        }
+
+        req.session.curr_user_id=curr_user_id
+
+       
+
+    res.render('layouts/home',{
+      loginid:req.session.loginid,
+      loggedin:req.session.loggedin,
+      username:req.session.username,
+      allusers:allusers,
+      curr_user_id:curr_user_id,
+      })
+  }else{
+    res.redirect('/myaccount')
+  }
+})
 
 module.exports = router;
