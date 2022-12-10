fe485608d1b6e3047fc5de8964a003a6907a4d7e
Lensikoviski <lensikoviski@gmail.com>
2022-12-10 08:09:08 +0530

    third commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+const { MongoClient } = require('mongodb');
+const uri="mongodb+srv://jayaramskumar:just4marry@cluster0.gguofay.mongodb.net/matrimony?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
+
+
+
+router.get('/',async function(req, res, next) {
+  let data =  await client.db().collection('userinfo').findOne({name:'akhil U Nair'})
+  res.render('index', { title: 'Express' ,data:data});
 });
 
 module.exports = router;
