c942e4a083490ce0c509e2ce4cd7c3ab078e5f70
Lensikoviski <lensikoviski@gmail.com>
2022-12-10 11:20:04 +0530

    cache clear  added

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); 
 
+
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+}); 
+
 // view engine setup
 
 var {engine} = require('express-handlebars')
